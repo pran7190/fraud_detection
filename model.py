a487a9157b57fc92cdc1cf6ea27bc5a623b8b06e
pran7190 <pranit.somne@gmail.com>
2024-09-11 21:12:16 +0530

    code for model1 model2 and model 3 added

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("add all the ml related code here")
+
+def model1():
+    print("code for model1")
+
+def model2():
+    print("code for model2")
+
+def model3():
+    print("code for model3")
